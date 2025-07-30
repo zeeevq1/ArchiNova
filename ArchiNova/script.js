545fa547a5182fd15b94be9a7413bc5f8db57d99
Mateusz Olszewski <zeeevq1@gmail.com>
2025-07-30 08:39:41 +0200

    project complete

--- ArchiNova/script.js
+++ ArchiNova/script.js
@@ -1,9 +1,9 @@
-const toggleBtn = document.getElementById("menuToggle");
+const toggleBtn1 = document.getElementById("menuToggle");
 const menu = document.getElementById("mobileMenu");
 
 let isOpen = false;
 
-toggleBtn.addEventListener("click", () => {
+toggleBtn1.addEventListener("click", () => {
   isOpen = !isOpen;
 
   if (isOpen) {
@@ -23,5 +23,19 @@ toggleBtn.addEventListener("click", () => {
       "ease-out"
     );
     menu.classList.remove("translate-y-0", "opacity-100");
+  }
+});
+
+const toggleBtn = document.getElementById("darkToggle");
+
+const body = document.body;
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    body.classList.remove("bg-gray-100", "text-gray-900");
+    body.classList.add("bg-gray-900", "text-white");
+  } else {
+    body.classList.remove("bg-gray-900", "text-white");
+    body.classList.add("text-gray-900");
   }
 });
